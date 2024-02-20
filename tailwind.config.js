3288219a03984a7a9928322f33a48a3761272d3e
LeanNaikim <leangnaikim168@gmail.com>
2024-02-20 23:08:01 +0700

    new code

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#511F52',
+      secondary: '#693B69',
+    },
+  },
+  plugins: [],
+}
